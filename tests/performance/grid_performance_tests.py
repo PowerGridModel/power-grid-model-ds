6278edecc22de3d5a0a86fbc8032162803907f38
Thijs Baaijen <13253091+Thijss@users.noreply.github.com>
2025-02-04 00:33:14 +0100

    add perf test for add_lines
    Signed-off-by: Thijs Baaijen <13253091+Thijss@users.noreply.github.com>

--- tests/performance/grid_performance_tests.py
+++ tests/performance/grid_performance_tests.py
@@ -1,23 +1,40 @@
 # SPDX-FileCopyrightText: Contributors to the Power Grid Model project <powergridmodel@lfenergy.org>
 #
 # SPDX-License-Identifier: MPL-2.0
+
 from tests.performance._helpers import do_performance_test
 
 # pylint: disable=missing-function-docstring
 
+
 def perf_test_add_nodes():
     setup_code = {
-        "grid" : "from power_grid_model_ds import Grid;"
+        "grid": "from power_grid_model_ds import Grid;"
         + "from power_grid_model_ds._core.model.arrays import NodeArray;"
         + "grid = Grid.empty();"
         + "nodes = NodeArray.zeros({size});"
     }
 
-    code_to_test = [
-        "grid.append(nodes);"
-    ]
+    code_to_test = ["grid.append(nodes);"]
 
-    do_performance_test(code_to_test, [10, 1000, 5000], 100, setup_code)
+    do_performance_test(code_to_test, [10, 200, 1000], 100, setup_code)
+
+
+def perf_test_add_lines():
+    setup_code = {
+        "grid": "from power_grid_model_ds import Grid;"
+        + "from power_grid_model_ds._core.model.arrays import NodeArray, LineArray;"
+        + "grid = Grid.empty();"
+        + "nodes = NodeArray.zeros({size});"
+        + "grid.append(nodes);"
+        + "lines = LineArray.zeros({size});"
+        + "lines.from_node = nodes.id;"
+        + "lines.to_node = nodes.id;"
+    }
+
+    code_to_test = ["grid.append(lines);"]
+
+    do_performance_test(code_to_test, [10, 200, 1000], 100, setup_code)
 
 
 def perf_test_get_downstream_nodes_performance():
@@ -40,5 +57,6 @@ def perf_test_get_downstream_nodes_performance():
 
 
 if __name__ == "__main__":
-    # perf_test_get_downstream_nodes_performance()
+    perf_test_get_downstream_nodes_performance()
     perf_test_add_nodes()
+    perf_test_add_lines()
