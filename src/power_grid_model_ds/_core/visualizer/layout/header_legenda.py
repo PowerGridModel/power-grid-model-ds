2e71c45dd080ccb2715755bc3da7e949e9662293
Thijs Baaijen <13253091+Thijss@users.noreply.github.com>
2025-08-06 22:08:00 +0200

    use constant for bootstrap class
    Signed-off-by: Thijs Baaijen <13253091+Thijss@users.noreply.github.com>

--- src/power_grid_model_ds/_core/visualizer/layout/header_legenda.py
+++ src/power_grid_model_ds/_core/visualizer/layout/header_legenda.py
@@ -7,6 +7,7 @@ from dash import html
 
 from power_grid_model_ds._core.visualizer.layout.colors import CYTO_COLORS
 
+_BOOTSTRAP_ARROW_ICON_CLASS = "fas fa-arrow-right-long"
 _MARGIN = "0 10px"
 
 LEGENDA_STYLE = {"margin": _MARGIN, "text-shadow": "0 0 5px #000", "justify-content": "center"}
@@ -24,11 +25,11 @@ LEGENDA_ELEMENTS = [
     dbc.Tooltip("Node", target="node-icon", placement="bottom"),
     html.I(className="fas fa-diamond", id="substation-icon", style=_SUBSTATION_ICON_STYLE),
     dbc.Tooltip("Substation", target="substation-icon", placement="bottom"),
-    html.I(className="fas fa-arrow-right-long", id="line-icon", style=_LINE_ICON_STYLE),
+    html.I(className=_BOOTSTRAP_ARROW_ICON_CLASS, id="line-icon", style=_LINE_ICON_STYLE),
     dbc.Tooltip("Line", target="line-icon", placement="bottom"),
-    html.I(className="fas fa-arrow-right-long", id="transformer-icon", style=_TRANSFORMER_ICON_STYLE),
+    html.I(className=_BOOTSTRAP_ARROW_ICON_CLASS, id="transformer-icon", style=_TRANSFORMER_ICON_STYLE),
     dbc.Tooltip("Transformer", target="transformer-icon", placement="bottom"),
-    html.I(className="fas fa-arrow-right-long", id="link-icon", style=_LINK_ICON_STYLE),
+    html.I(className=_BOOTSTRAP_ARROW_ICON_CLASS, id="link-icon", style=_LINK_ICON_STYLE),
     dbc.Tooltip("Link", target="link-icon", placement="bottom"),
     html.I(className="fas fa-ellipsis", id="open-branch-icon", style=_OPEN_BRANCH_ICON_STYLE),
     dbc.Tooltip("Open Branch", target="open-branch-icon", placement="bottom"),
