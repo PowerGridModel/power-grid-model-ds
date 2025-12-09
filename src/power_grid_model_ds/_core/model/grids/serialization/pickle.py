93b83bdac42be4055c8b07ce706e1ad49f0585f9
Thijs Baaijen <13253091+Thijss@users.noreply.github.com>
2025-12-09 11:49:12 +0100

    Add reuse compliance
    Signed-off-by: Thijs Baaijen <13253091+Thijss@users.noreply.github.com>

--- src/power_grid_model_ds/_core/model/grids/serialization/pickle.py
+++ src/power_grid_model_ds/_core/model/grids/serialization/pickle.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2025 Contributors to the Power Grid Model project <powergridmodel@lfenergy.org>
+#
+# SPDX-License-Identifier: MPL-2.0
+
 from copy import copy
 from pathlib import Path
 from typing import TYPE_CHECKING
