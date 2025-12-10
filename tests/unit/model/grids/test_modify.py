7d557851e4c55ae5d5d74a94da438b83b0d9b02d
Thijs Baaijen <13253091+Thijss@users.noreply.github.com>
2025-12-10 22:00:54 +0100

    Add reuse compliance
    Signed-off-by: Thijs Baaijen <13253091+Thijss@users.noreply.github.com>

--- tests/unit/model/grids/test_modify.py
+++ tests/unit/model/grids/test_modify.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: Contributors to the Power Grid Model project <powergridmodel@lfenergy.org>
+#
+# SPDX-License-Identifier: MPL-2.0
 from power_grid_model_ds import Grid
 from power_grid_model_ds._core.model.arrays import (
     LineArray,
