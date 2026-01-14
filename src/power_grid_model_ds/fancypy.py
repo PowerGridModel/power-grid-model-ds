be138d31f225ab0fdda291d6abc1a5e57de48cbc
Thijs Baaijen <13253091+Thijss@users.noreply.github.com>
2026-01-14 16:31:00 +0100

    Apply suggestion from @Thijss
    Signed-off-by: Thijs Baaijen <13253091+Thijss@users.noreply.github.com>

--- src/power_grid_model_ds/fancypy.py
+++ src/power_grid_model_ds/fancypy.py
@@ -7,4 +7,4 @@ from power_grid_model_ds._core.model.arrays.base.array import FancyArray
 from power_grid_model_ds._core.model.containers.base import FancyArrayContainer
 from power_grid_model_ds._core.model.containers.helpers import container_equal
 
-__all__ = ["FancyArray", "FancyArrayContainer", "concatenate", "unique", "sort", "array_equal", "container_equal"]
+__all__ = ["FancyArray", "FancyArrayContainer", "concatenate", "unique", "sort", "array_equal"]
