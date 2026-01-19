29aabfae868265e21092743313de865b68e4f4b6
Thijs Baaijen <13253091+Thijss@users.noreply.github.com>
2026-01-19 13:01:57 +0100

    Add reuse comments
    Signed-off-by: Thijs Baaijen <13253091+Thijss@users.noreply.github.com>

--- src/power_grid_model_ds/_core/model/grids/serialization/errors.py
+++ src/power_grid_model_ds/_core/model/grids/serialization/errors.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: Contributors to the Power Grid Model project <powergridmodel@lfenergy.org>
+#
+# SPDX-License-Identifier: MPL-2.0
+
 class JSONSerializationError(Exception):
     """Exception raised for errors during JSON serialization of grid attributes."""
 
