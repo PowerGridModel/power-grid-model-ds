1add36ce8541be958b55adb716873f8d27ee8cac
wander.wadman <wander.wadman@alliander.com>
2026-01-21 15:53:25 +0100

    Moved tests to test_helpers
    Signed-off-by: wander.wadman <wander.wadman@alliander.com>

---
+++ tests/unit/model/grids/test_helpers.py
@@ -0,0 +1,41 @@
+from power_grid_model_ds import Grid
+from power_grid_model_ds._core.model.arrays import SourceArray
+
+
+class TestMergeGrids:
+    def test_merge_two_grids(self):
+        grid1 = Grid.from_txt("S1 2", "S1 3 link", "3 4 transformer")
+        grid2 = Grid.from_txt("S11 12", "S11 13 link", "13 14 transformer")
+        grid1_size = grid1.node.size
+        grid2_size = grid2.node.size
+
+        grid1.merge(grid2, mode="keep_ids")
+        merged_grid_size = grid1.node.size
+
+        assert merged_grid_size == grid1_size + grid2_size, "Merged grid size should be the sum of both grids' sizes"
+
+    def test_merge_two_grids_with_overlapping_node_ids(self):
+        grid1 = Grid.from_txt("S1 2", "S1 3 link", "3 4 transformer")
+        grid2 = Grid.from_txt("S1 2", "S1 13 link", "13 14 transformer")
+        source = SourceArray(id=[501], node=[1], status=[1], u_ref=[0.0])
+        grid1.append(source)
+        grid2.append(source)
+
+        grid1.merge(grid2, mode="recalculate_ids")
+        assert grid1.check_ids() is None, "Asset ids are not unique after merging!"
+
+        # Check if from and to nodes are updated by checking that their values form the entire set of node ids:
+        assert set(grid1.branches.from_node).union(grid1.branches.to_node) == set(grid1.node.id), (
+            "All from and to nodes should form the entire set of node ids in the merged grid!"
+        )
+
+        # assert node in grid.source is updated by checking if the node column contains values that are all node ids:
+        assert set(grid1.source.node).issubset(grid1.node.id), "All source nodes should be valid node ids!"
+
+    def test_merge_two_grids_with_overlapping_line(self):
+        # Now both grids have 14 as highest node id, so both will have branch ids 15, 16 and 17:
+        grid1 = Grid.from_txt("S1 2", "S1 3 link", "3 14 transformer")
+        grid2 = Grid.from_txt("S1 2", "S1 13 link", "13 14 transformer")
+
+        grid1.merge(grid2, mode="recalculate_ids")
+        assert grid1.check_ids() is None, "Asset ids are not unique after merging!"
