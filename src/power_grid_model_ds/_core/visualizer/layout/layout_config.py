ee2e92c7d464e115a6d5b2894676fb07b4df32c4
Nitish Bharambe <nitish.bharambe@alliander.com>
2026-01-30 15:01:31 +0100

    fix reuse
    Signed-off-by: Nitish Bharambe <nitish.bharambe@alliander.com>

--- src/power_grid_model_ds/_core/visualizer/layout/layout_config.py
+++ src/power_grid_model_ds/_core/visualizer/layout/layout_config.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: Contributors to the Power Grid Model project <powergridmodel@lfenergy.org>
+#
+# SPDX-License-Identifier: MPL-2.0
+
 from power_grid_model_ds._core.model.arrays.pgm_arrays import NodeArray
 from power_grid_model_ds._core.model.enums.nodes import NodeType
 
