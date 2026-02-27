09f3cc5cf8df09f4f6b6a9a434a8b5e685c9fc6b
Nitish Bharambe <nitish.bharambe@alliander.com>
2026-02-27 12:07:02 +0100

    remove qmin max
    Signed-off-by: Nitish Bharambe <nitish.bharambe@alliander.com>

--- src/power_grid_model_ds/_core/model/dtypes/regulators.py
+++ src/power_grid_model_ds/_core/model/dtypes/regulators.py
@@ -34,5 +34,3 @@ class VoltageRegulator(Regulator):
     """Voltage regulator data type"""
 
     u_ref: NDArray[np.float64]  # the voltage setpoint
-    q_max: NDArray[np.float64]  # the width of the voltage band
-    q_min: NDArray[np.float64]  # compensation for voltage drop due to resistance during transport
