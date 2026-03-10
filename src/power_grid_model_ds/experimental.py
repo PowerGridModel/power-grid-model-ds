16c52adef9f8b0ba35c3017c44477a6328843cf5
Thijs Baaijen <13253091+Thijss@users.noreply.github.com>
2026-03-10 11:08:56 +0100

    format
    Signed-off-by: Thijs Baaijen <13253091+Thijss@users.noreply.github.com>

--- src/power_grid_model_ds/experimental.py
+++ src/power_grid_model_ds/experimental.py
@@ -2,6 +2,7 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 """This module exposes functions/classes that are still under development and potentially subject to change."""
+
 from power_grid_model_ds._core.model.grids._search import find_differences_between_grids
 from power_grid_model_ds._core.utils.misc import find_diff_masks_with_equal_nan
 
