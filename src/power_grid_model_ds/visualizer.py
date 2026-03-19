deebac1242018bd1231a9bbd925828add75e0d0a
Thijs Baaijen <13253091+Thijss@users.noreply.github.com>
2026-03-19 11:50:31 +0100

    Move visualize import to visualizer.py
    Signed-off-by: Thijs Baaijen <13253091+Thijss@users.noreply.github.com>

--- src/power_grid_model_ds/visualizer.py
+++ src/power_grid_model_ds/visualizer.py
@@ -2,11 +2,44 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
+from power_grid_model_ds import Grid
+
 try:
-    from power_grid_model_ds._core.visualizer.app import visualize
+    from dash import Dash, dcc, html
+    import dash_bootstrap_components as dbc
+    from power_grid_model_ds._core.visualizer.app import get_app_layout, MDBOOTSTRAP, GOOGLE_FONTS
+    from dash_bootstrap_components.icons import FONT_AWESOME
 except ImportError as error:
     raise ImportError(
         "Missing dependencies for visualizer: install with 'pip install power-grid-model-ds[visualizer]'"
     ) from error
 
-__all__ = ["visualize"]
+
+
+def visualize(grid: Grid, debug: bool = False, port: int = 8050) -> None:
+    """Visualize the Grid.
+
+    grid: Grid
+        The grid to visualize.
+
+    layout: str
+        The layout to use.
+
+        If 'layout' is not provided (""):
+            And grid.node contains "x" and "y" columns:
+                The layout will be set to "preset" which uses the x and y coordinates to place the nodes.
+            Otherwise:
+                The layout will be set to "breadthfirst", which is a hierarchical breadth-first-search (BFS) layout.
+        Other options:
+            - "random": A layout that places the nodes randomly.
+            - "circle": A layout that places the nodes in a circle.
+            - "concentric": A layout that places the nodes in concentric circles.
+            - "grid": A layout that places the nodes in a grid matrix.
+            - "cose": A layout that uses the CompoundSpring Embedder algorithm (force-directed layout)
+    """
+
+    app = Dash(
+        external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP, MDBOOTSTRAP, FONT_AWESOME, GOOGLE_FONTS]
+    )
+    app.layout = get_app_layout(grid)
+    app.run(debug=debug, port=port)
