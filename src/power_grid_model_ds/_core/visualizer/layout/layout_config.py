ee621bfc591ce113d69c6915c0feb48889e4bf91
Thijs Baaijen <13253091+Thijss@users.noreply.github.com>
2026-03-20 14:00:03 +0100

    fix(visualizer): coordinates weren't working anymore in CONFIG menu (#192)
    * Fix bug with visualizing coordinates
    
    Signed-off-by: Thijs Baaijen <13253091+Thijss@users.noreply.github.com>
    
    * Apply suggestion from @nitbharambe
    
    Co-authored-by: Nitish Bharambe <78108900+nitbharambe@users.noreply.github.com>
    Signed-off-by: Thijs Baaijen <13253091+Thijss@users.noreply.github.com>
    
    * Update tests/unit/visualizer/test_layout.py
    
    Co-authored-by: Nitish Bharambe <78108900+nitbharambe@users.noreply.github.com>
    Signed-off-by: Thijs Baaijen <13253091+Thijss@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Thijs Baaijen <13253091+Thijss@users.noreply.github.com>
    Co-authored-by: Nitish Bharambe <78108900+nitbharambe@users.noreply.github.com>

--- src/power_grid_model_ds/_core/visualizer/layout/layout_config.py
+++ src/power_grid_model_ds/_core/visualizer/layout/layout_config.py
@@ -4,23 +4,24 @@
 
 
 from power_grid_model_ds._core.model.arrays.pgm_arrays import NodeArray
+from power_grid_model_ds._core.visualizer.layout.header_config import LayoutOptions
 
 
-def layout_with_config(layout_name, source_nodes) -> dict:
+def layout_with_config(layout: LayoutOptions, source_nodes: list[int]) -> dict:
     """Get the layout options for the selected layout."""
-    if layout_name == "breadthfirst":
+    if layout is LayoutOptions.BREADTHFIRST:
         config_dict = {
-            "name": layout_name,
+            "name": layout.value,
             "spacingFactor": 2.5,
         }
         if source_nodes:
             config_dict["roots"] = ", ".join(f'[id = "{node}"]' for node in source_nodes)
         return config_dict
-    return {"name": layout_name}
+    return {"name": layout.value}
 
 
-def get_default_graph_layout(nodes: NodeArray) -> str:
+def get_default_graph_layout(nodes: NodeArray) -> LayoutOptions:
     """Determine the graph layout"""
     if "x" in nodes.columns and "y" in nodes.columns:
-        return "preset"
-    return "breadthfirst"
+        return LayoutOptions.PRESET
+    return LayoutOptions.BREADTHFIRST
