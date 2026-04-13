4088d7d58b24b3816bd466bc8a366984170dce9e
Tom van de Poll <61697723+TomvdPoll@users.noreply.github.com>
2026-04-13 13:48:27 +0200

    Add explicit size property to FancyArray (#225)
    * Add explicit size property to FancyArray
    
    * DCO Remediation Commit for TomvandePoll <tom.van.de.poll@alliander.com>
    
    I, TomvandePoll <tom.van.de.poll@alliander.com>, hereby add my Signed-off-by to this commit: c287ddd18a5e73775ee0953ef9be37a2e9809471
    
    Signed-off-by: TomvandePoll <tom.van.de.poll@alliander.com>
    
    * cleanup
    
    * cleanup
    
    * Signed-off-by: TomvandePoll <tom.van.de.poll@alliander.com>
    
    signing off
    
    * break format
    
    Signed-off-by: TomvandePoll <tom.van.de.poll@alliander.com>
    
    * DCO Remediation Commit for TomvandePoll <tom.van.de.poll@alliander.com>
    
    I, TomvandePoll <tom.van.de.poll@alliander.com>, hereby add my Signed-off-by to this commit: 8acebc2111f950378ab3fe71fc8e005333d0f29e
    I, TomvandePoll <tom.van.de.poll@alliander.com>, hereby add my Signed-off-by to this commit: fd2148ea23afe986d25cfebdf19bb740b00bbeb2
    
    Signed-off-by: TomvandePoll <tom.van.de.poll@alliander.com>
    
    ---------
    
    Signed-off-by: TomvandePoll <tom.van.de.poll@alliander.com>

--- tests/unit/model/arrays/test_array.py
+++ tests/unit/model/arrays/test_array.py
@@ -316,3 +316,7 @@ def test_from_extended_array():
     array = LineArray.from_extended(extended_array)
     assert not isinstance(array, DefaultedCustomLineArray)
     array_equal_with_nan(array.data, extended_array[array.columns])
+
+
+def test_size(fancy_test_array: FancyTestArray):
+    assert fancy_test_array.size == 3
