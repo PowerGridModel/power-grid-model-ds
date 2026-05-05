a71e4522a86e93e01d20fd6ae0e2698625d20e18
Thijs Baaijen <13253091+Thijss@users.noreply.github.com>
2026-05-05 14:53:33 +0200

    Add ExtraArray to ExtendedGridFixture
    Signed-off-by: Thijs Baaijen <13253091+Thijss@users.noreply.github.com>

--- tests/fixtures/grid_classes.py
+++ tests/fixtures/grid_classes.py
@@ -3,9 +3,22 @@
 # SPDX-License-Identifier: MPL-2.0
 
 from dataclasses import dataclass
+from typing import ClassVar
+
+import numpy as np
+from numpy._typing import NDArray
 
 from power_grid_model_ds._core.model.grids.base import Grid
+from power_grid_model_ds.arrays import IdArray
 from tests.fixtures.arrays import CustomLineArray, CustomNodeArray, DefaultedCustomLineArray, DefaultedCustomNodeArray
+
+class ExtraArray(IdArray):
+    """An extra array with referencing id columns."""
+    branch_id: NDArray[np.int32]
+    node_id: NDArray[np.int32]
+
+    _id_columns: ClassVar[set[str]] = {"branch_id", "node_id"}
+    _defaults: ClassVar[dict[str, int]] = {"branch_id": 42, "node_id": 43}
 
 
 @dataclass
@@ -14,6 +27,7 @@ class ExtendedGrid(Grid):
 
     node: DefaultedCustomNodeArray
     line: DefaultedCustomLineArray
+    extra: ExtraArray
     extra_value: int = 123
 
 
